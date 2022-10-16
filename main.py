e4e279e6fcf5805c33f6928116181b76e6585490
Name <email@example.ru>
2022-10-16 23:46:53 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from pygame.math import Vector2
 import time
 import random
 import sys
-from enum import Enum
 
 snake_speed = 20
 
@@ -13,19 +12,10 @@ RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 
 
-class SnakeDirection(Enum):
-    RIGHT = 'RIGHT'
-    LEFT = 'LEFT'
-    DOWN = 'DOWN'
-    UP = 'UP'
-
-
 class Snake:
     def __init__(self):
-        # количество блоков в змейке
-        # выделить голову цветом
         self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10), Vector2(2, 10)]
-        self.dir = Vector2(0, 0)
+        self.dir = Vector2(1, 0)
         self.new_block = False
 
     def draw_snake(self):
@@ -38,13 +28,25 @@ class Snake:
     # остальные блоки появляются и исчезают на месте головы, которая появляется с определенной скоростью.
 
     def move_snake(self):
-        body_copy = self.body[:-1]
-        body_copy.insert(0, body_copy[0] + self.dir)
-        self.body = body_copy[:]
+        if self.new_block is True:
+            body_copy = self.body[:]
+            body_copy.insert(0, body_copy[0] + self.dir)
+            self.body = body_copy[:]
+            self.new_block = False
+        else:
+            body_copy = self.body[:-1]
+            body_copy.insert(0, body_copy[0] + self.dir)
+            self.body = body_copy[:]
+
+    def add_block(self):
+        self.new_block = True
 
 
 class Apple:
     def __init__(self):
+        self.x = random.randint(0, BLOCK_NUMBER - 1)
+        self.y = random.randint(0, BLOCK_NUMBER - 1)
+        self.pos = Vector2(self.x, self.y)
         self.randomize()
 
     def draw_apple(self):
@@ -57,21 +59,70 @@ class Apple:
         self.pos = Vector2(self.x, self.y)
 
 
-pygame.init()
+class Game:
+    def __init__(self):
+        self.snake = Snake()
+        self.apple = Apple()
 
+    def update(self):
+        self.snake.move_snake()
+        self.check_collision()
+        self.check_game_over()
+
+    def draw_all(self):
+        self.apple.draw_apple()
+        self.snake.draw_snake()
+        self.score()
+
+    def check_collision(self):
+        if self.apple.pos == self.snake.body[0]:
+            self.apple.randomize()
+            self.snake.add_block()
+
+        for block in self.snake.body[1:]:
+            if block == self.apple.pos:
+                self.apple.randomize()      # если яблочко появляется в теле змейки, то пусть появится еще раз))
+
+    def check_game_over(self):
+        if not 0 <= self.snake.body[0].x < BLOCK_NUMBER or not 0 <= self.snake.body[0].y < BLOCK_NUMBER:
+            self.game_over()
+            # буквально: проверяем голову змейки, находится ли они между нулем и количеством клеток поля.
+            # Если нет, значит, врезалась
+            # У меня 50 клеточек. Но <= нельзя, иначе змейка высовывает голову за поле
+        for block in self.snake.body[1:]:
+            if block == self.snake.body[0]:
+                self.game_over()
+                # если блоки змейки, находящиеся после ее головы - это [1:] - как бы равняются с другими блоками змейки
+                # то произошел самокусь и мы проиграли
+
+    def game_over(self):
+        score_text = len(self.snake.body) - 4
+        print('Ваш счет: ' + str(score_text), True, RED)
+
+    def score(self):
+        score_text = str(len(self.snake.body) - 4)
+        score_surface = font.render('Счет : ' + str(score_text), True, WHITE)
+        score_x = int(BLOCK_SIZE * BLOCK_NUMBER - 60)
+        score_y = int(BLOCK_SIZE * BLOCK_NUMBER - 40)
+        score_rect = score_surface.get_rect(center=(score_x, score_y))
+        screen.blit(score_surface, score_rect)
+        pygame.draw.rect(screen, (167, 209, 61), score_rect)
+        pygame.draw.rect(screen, (56, 74, 12), score_rect, 2)
+
+
+pygame.init()
 
 BLOCK_SIZE = 15
 BLOCK_NUMBER = 50
-
 pygame.display.set_caption('Змейка')
 screen = pygame.display.set_mode((BLOCK_SIZE * BLOCK_NUMBER, BLOCK_SIZE * BLOCK_NUMBER))
 clock = pygame.time.Clock()
-
-apple = Apple()
-snake = Snake()
+font = pygame.font.Font(None, 25)
 
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 150) # в МИЛИСЕКУНДАХ!
+
+main_game = Game()
 
 while True:
     # чтобы закрывать на крестик
@@ -81,98 +132,30 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == SCREEN_UPDATE:
-            snake.move_snake()
-            # управление
+            main_game.update()
+            # управление.
+            # if внутри проверяет движение змейки в саму себя и не дает этому случиться
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                snake.dir = Vector2(0, -1)
+                if main_game.snake.dir.y != 1:
+                    main_game.snake.dir = Vector2(0, -1)
             if event.key == pygame.K_RIGHT:
-                snake.dir = Vector2(1, 0)
+                if main_game.snake.dir.x != -1:
+                    main_game.snake.dir = Vector2(1, 0)
             if event.key == pygame.K_DOWN:
-                snake.dir = Vector2(0, 1)
+                if main_game.snake.dir.y != -1:
+                    main_game.snake.dir = Vector2(0, 1)
             if event.key == pygame.K_LEFT:
-                snake.dir = Vector2(-1, 0)
+                if main_game.snake.dir.x != 1:
+                    main_game.snake.dir = Vector2(-1, 0)
 
     screen.fill(FRAME_COLOR)
-    apple.draw_apple()
-    snake.draw_snake()
+    main_game.draw_all()
     pygame.display.update()
     clock.tick(60)
 
-score = 0
-
-# это пока не работает
-class Score:
-    def __init__(self, color, font, size) -> None:
-        # считаем
-        score_font = pygame.font.SysFont(font, size)
-        # показываем
-        # ДОДЕЛАТЬ! А то не показываем
-        score_surface = score_font.render('Счет : ' + str(score), True, color)
-        score_rect = score_surface.get_rect()
-        screen.blit(score_surface, score_rect)
-        pygame.draw.rect(screen, (167, 209, 61), score_rect)
-        pygame.draw.rect(screen, (56, 74, 12), score_rect, 2)
-
-    # и это
-    def game_over(self):
-        my_font = pygame.font.SysFont('times new roman', 50)
-        game_over_surface = my_font.render(
-            'Ваш счет: ' + str(score), True, RED)
-
-        game_over_rect = game_over_surface.get_rect()
-
-        # расположение текста
-        game_over_rect.midtop = ((200, 50))
-        screen.blit(game_over_surface, game_over_rect)
-        pygame.display.flip()
-
-        time.sleep(2)
-        pygame.quit()
-        quit()
-
-
-
-
-#             # старое управление
-#         if event.type == pygame.KEYDOWN:
-#             if event.key == pygame.K_UP:
-#                 snake.turn_up()
-#             if event.key == pygame.K_DOWN:
-#                 snake.turn_down()
-#             if event.key == pygame.K_LEFT:
-#                 snake.turn_left()
-#             if event.key == pygame.K_RIGHT:
-#                 snake.turn_right()
-#
-#     # проверка на диагональное движение
-#     if change_to == 'UP' and direction != 'DOWN':
-#         direction = 'UP'
-#     if change_to == 'DOWN' and direction != 'UP':
-#         direction = 'DOWN'
-#     if change_to == 'LEFT' and direction != 'RIGHT':
-#         direction = 'LEFT'
-#     if change_to == 'RIGHT' and direction != 'LEFT':
-#         direction = 'RIGHT'
-#
-#     # позлем!
-#     if direction == 'UP':
-#         snake_position[1] -= 10
-#     if direction == 'DOWN':
-#         snake_position[1] += 10
-#     if direction == 'LEFT':
-#         snake_position[0] -= 10
-#     if direction == 'RIGHT':
-#         snake_position[0] += 10
-#         # TO DO
-#     # тело змейки удлинняется, когда она ест. Очки начисляются за яблочки
-#     snake_body.insert(0, list(snake_position))
-#     if snake_position[0] == apple.x and snake_position[1] == apple.y:
-#         score += 10
-#         apple.respawn()
-#     else:
-#         snake_body.pop()
-#
-#
-#
+# шпаргалка:
+# v = Vector2(5, 4)               Is = [5, 4]
+# v.x -> 5                        Is[0] -> 5
+# v.y -> 4                        Is[1] -> 4
 
